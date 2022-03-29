82f43b7e69621827b17b6842987b293f41f48bc1
S3B4SZ17 <sebaszh17@gmail.com>
2022-03-28 19:53:54 -0600

    :pencil: Feat: Add the initial code with Java and Springboot for the app connection

---
+++ oracle_db/src/main/java/com/java_oracle/project/oracle_db/OracleDbApplication.java
@@ -0,0 +1,13 @@
+package com.java_oracle.project.oracle_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OracleDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OracleDbApplication.class, args);
+	}
+
+}
---
+++ oracle_db/src/test/java/com/java_oracle/project/oracle_db/OracleDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java_oracle.project.oracle_db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OracleDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
