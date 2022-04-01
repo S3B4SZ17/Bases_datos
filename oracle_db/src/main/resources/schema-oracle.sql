e654228c450a27f8534700239ff889a68764e974
S3B4SZ17 <sebaszh17@gmail.com>
2022-03-31 22:58:27 -0600

    :pencil: Feat: Fix some errors in the SQL syntax of the script

--- oracle_db/src/main/resources/schema-oracle.sql
+++ oracle_db/src/main/resources/schema-oracle.sql
@@ -36,7 +36,7 @@ CREATE TABLE Staff (
 	Salario INT NOT NULL,
 	constraint STAFF_PK PRIMARY KEY (idStaff));
 
-CREATE TABLE Cuentas_IBAN (
+CREATE TABLE Cuentas_IBAN (	
 	idCuenta INT NOT NULL,
 	Staff INT NOT NULL,
 	constraint CUENTAS_IBAN_PK PRIMARY KEY (idCuenta));
@@ -53,78 +53,6 @@ CREATE TABLE Salarios (
 	Cuenta_IBAN INT NOT NULL,
 	constraint SALARIOS_PK PRIMARY KEY (idSalario));
 
-CREATE sequence CLIENTES_IDCLIENTE_SEQ;
-
-CREATE trigger BI_CLIENTES_IDCLIENTE
-  before insert on Clientes
-  for each row
-begin
-  select CLIENTES_IDCLIENTE_SEQ.nextval into :NEW.idCliente from dual;
-end;
-
-CREATE sequence MATRICULA_IDMATRICULA_SEQ;
-
-CREATE trigger BI_MATRICULA_IDMATRICULA
-  before insert on Matricula
-  for each row
-begin
-  select MATRICULA_IDMATRICULA_SEQ.nextval into :NEW.idMatricula from dual;
-end;
-
-CREATE sequence SUSCRIPCIONES_IDSUSCRIPCION_SEQ;
-
-CREATE trigger BI_SUSCRIPCIONES_IDSUSCRIPCION
-  before insert on Suscripciones
-  for each row
-begin
-  select SUSCRIPCIONES_IDSUSCRIPCION_SEQ.nextval into :NEW.idSuscripcion from dual;
-end;
-
-CREATE sequence NUTRICION_IDNUTRICION_SEQ;
-
-CREATE trigger BI_NUTRICION_IDNUTRICION
-  before insert on Nutricion
-  for each row
-begin
-  select NUTRICION_IDNUTRICION_SEQ.nextval into :NEW.idNutricion from dual;
-end;
-
-CREATE sequence STAFF_IDSTAFF_SEQ;
-
-CREATE trigger BI_STAFF_IDSTAFF
-  before insert on Staff
-  for each row
-begin
-  select STAFF_IDSTAFF_SEQ.nextval into :NEW.idStaff from dual;
-end;
-
-CREATE sequence HORARIOS_IDHORARIO_SEQ;
-
-CREATE trigger BI_HORARIOS_IDHORARIO
-  before insert on Horarios
-  for each row
-begin
-  select HORARIOS_IDHORARIO_SEQ.nextval into :NEW.idHorario from dual;
-end;
-
-CREATE sequence SALARIOS_IDSALARIO_SEQ;
-
-CREATE trigger BI_SALARIOS_IDSALARIO
-  before insert on Salarios
-  for each row
-begin
-  select SALARIOS_IDSALARIO_SEQ.nextval into :NEW.idSalario from dual;
-end;
-
-CREATE sequence CUENTAS_IBAN_IDCUENTA_SEQ;
-
-CREATE trigger BI_CUENTAS_IBAN_IDCUENTA
-  before insert on Cuentas_IBAN
-  for each row
-begin
-  select CUENTAS_IBAN_IDCUENTA_SEQ.nextval into :NEW.idCuenta from dual;
-end;
-
 ALTER TABLE Matricula ADD CONSTRAINT Matricula_fk0 FOREIGN KEY (Cliente) REFERENCES Clientes(idCliente);
 ALTER TABLE Matricula ADD CONSTRAINT Matricula_fk1 FOREIGN KEY (Suscripcion) REFERENCES Suscripciones(idSuscripcion);
 ALTER TABLE Matricula ADD CONSTRAINT Matricula_fk2 FOREIGN KEY (Nutricion) REFERENCES Nutricion(idNutricion);
@@ -135,3 +63,74 @@ ALTER TABLE Staff ADD CONSTRAINT Staff_fk0 FOREIGN KEY (Salario) REFERENCES Sala
 ALTER TABLE Salarios ADD CONSTRAINT Salarios_fk0 FOREIGN KEY (Cuenta_IBAN) REFERENCES Cuentas_IBAN(idCuenta);
 ALTER TABLE Cuentas_IBAN ADD CONSTRAINT Cuentas_IBAN_fk0 FOREIGN KEY (Staff) REFERENCES Staff(idStaff);
 
+CREATE sequence CLIENTES_IDCLIENTE_SEQ;
+
+CREATE sequence MATRICULA_IDMATRICULA_SEQ;
+
+CREATE sequence SUSCRIPCIONES_IDSUSCRIPCION_SEQ;
+
+CREATE sequence NUTRICION_IDNUTRICION_SEQ;
+
+CREATE sequence STAFF_IDSTAFF_SEQ;
+
+CREATE sequence HORARIOS_IDHORARIO_SEQ;
+
+CREATE sequence CUENTAS_IBAN_IDCUENTA_SEQ;
+
+CREATE sequence SALARIOS_IDSALARIO_SEQ;
+
+CREATE OR REPLACE trigger BI_CLIENTES_IDCLIENTE
+  before insert on Clientes
+  for each row
+begin
+  select CLIENTES_IDCLIENTE_SEQ.nextval into :NEW.idCliente from dual
+end;
+
+CREATE OR REPLACE trigger BI_MATRICULA_IDMATRICULA
+  before insert on Matricula
+  for each row
+begin
+  select MATRICULA_IDMATRICULA_SEQ.nextval into :NEW.idMatricula from dual
+end;
+
+CREATE OR REPLACE trigger BI_SUSCRIPCIONES_IDSUSCRIPCION
+  before insert on Suscripciones
+  for each row
+begin
+  select SUSCRIPCIONES_IDSUSCRIPCION_SEQ.nextval into :NEW.idSuscripcion from dual
+end;
+
+CREATE OR REPLACE trigger BI_NUTRICION_IDNUTRICION
+  before insert on Nutricion
+  for each row
+begin
+  select NUTRICION_IDNUTRICION_SEQ.nextval into :NEW.idNutricion from dual
+end;
+
+CREATE OR REPLACE trigger BI_STAFF_IDSTAFF
+  before insert on Staff
+  for each row
+begin
+  select STAFF_IDSTAFF_SEQ.nextval into :NEW.idStaff from dual
+end;
+
+CREATE OR REPLACE trigger BI_HORARIOS_IDHORARIO
+  before insert on Horarios
+  for each row
+begin
+  select HORARIOS_IDHORARIO_SEQ.nextval into :NEW.idHorario from dual
+end;
+
+CREATE OR REPLACE trigger BI_SALARIOS_IDSALARIO
+  before insert on Salarios
+  for each row
+begin
+  select SALARIOS_IDSALARIO_SEQ.nextval into :NEW.idSalario from dual
+end;
+
+CREATE OR REPLACE trigger BI_CUENTAS_IBAN_IDCUENTA
+  before insert on Cuentas_IBAN
+  for each row
+begin
+  select CUENTAS_IBAN_IDCUENTA_SEQ.nextval into :NEW.idCuenta from dual
+end;
