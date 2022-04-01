7c8828c41af347a65c5a556974853b4d9a3620cf
S3B4SZ17 <sebaszh17@gmail.com>
2022-03-31 21:35:44 -0600

    :pencil: Feat: Add the connection to the database

--- README.md
+++ README.md
@@ -1,9 +1,23 @@
 # Bases_datos
+
 Proyecto final de bases de datos
 
 ## Minuta de Planeacion de Proyecto Entregable 1:
+
 - El Proyecto definido sera el tema de Gimnasio, el cual sera una aplicacion que se conectara a una base de datos ORACLE para la extraccion de los datos.
--	La aplicacion se desarrollara en lenguaje JAVA utilizando el framework de SpringBoot
+- La aplicacion se desarrollara en lenguaje JAVA utilizando el framework de SpringBoot
 - La base de datos contara con un total aproximado de 8 tablas. El total de tablas puede variar a través de la longitud del Proyecto y dependiendo de las características adicionales que se deseen agregar.
 - El Proyecto contara con un repositorio GitHub en donde los miembros del grupo agregaremos nuestros aportes según sea necesario.
 - El repositorio GitHub creado es identificado con el [Link](https://github.com/S3B4SZ17/Bases_datos)
+
+## Pasos para crear usuario en la base de datos
+
+`CREATE USER sebas IDENTIFIED BY SebasZumbado1234;`
+
+Despues para que el usuario se pueda conectar hay que asignarle ciertos permisos:
+
+- `GRANT CREATE SESSION TO sebas;`
+- `GRANT CREATE TABLE TO sebas;`
+- Para obetner el default tablespace: `SELECT default_tablespace FROM dba_users WHERE username = 'sebas';`
+- `ALTER USER sebas QUOTA UNLIMITED ON USERS;`
+- Para tener permisos de _crear vies_, _crear procedures_, _crear sequencias_: `GRANT create view, create procedure, create sequence TO sebas;`
---
+++ oracle_db/src/main/resources/schema-oracle.sql
@@ -0,0 +1,137 @@
+CREATE TABLE Clientes (
+	idCliente INT NOT NULL,
+	Nombre VARCHAR2(20) NOT NULL,
+	Apellido VARCHAR2(20) NOT NULL,
+	Meses_Activos INT NOT NULL,
+	constraint CLIENTES_PK PRIMARY KEY (idCliente));
+
+CREATE TABLE Matricula (
+	idMatricula INT NOT NULL,
+	Cliente INT NOT NULL,
+	Suscripcion INT NOT NULL,
+	Nutricion INT NOT NULL,
+	constraint MATRICULA_PK PRIMARY KEY (idMatricula));
+
+CREATE TABLE Suscripciones (
+	idSuscripcion INT NOT NULL,
+	Nombre VARCHAR2(20) NOT NULL,
+	Description VARCHAR2(500) NOT NULL,
+	Entrenador INT NOT NULL,
+	Horario INT NOT NULL,
+	Precio FLOAT(100) NOT NULL,
+	constraint SUSCRIPCIONES_PK PRIMARY KEY (idSuscripcion));
+
+CREATE TABLE Nutricion (
+	idNutricion INT NOT NULL,
+	Nombre VARCHAR2(20) NOT NULL,
+	Descripcion VARCHAR2(500) NOT NULL,
+	Nutricionista INT NOT NULL,
+	constraint NUTRICION_PK PRIMARY KEY (idNutricion));
+
+CREATE TABLE Staff (
+	idStaff INT NOT NULL,
+	Nombre VARCHAR2(20) NOT NULL,
+	Apellido VARCHAR2(20) NOT NULL,
+	Especialidad VARCHAR2(50) NOT NULL,
+	Salario INT NOT NULL,
+	constraint STAFF_PK PRIMARY KEY (idStaff));
+
+CREATE TABLE Cuentas_IBAN (
+	idCuenta INT NOT NULL,
+	Staff INT NOT NULL,
+	constraint CUENTAS_IBAN_PK PRIMARY KEY (idCuenta));
+
+CREATE TABLE Horarios (
+	idHorario INT NOT NULL,
+	Nombre VARCHAR2(20) NOT NULL,
+	Descripcion VARCHAR2(500) NOT NULL,
+	constraint HORARIOS_PK PRIMARY KEY (idHorario));
+
+CREATE TABLE Salarios (
+	idSalario INT NOT NULL,
+	Monto FLOAT(100) NOT NULL,
+	Cuenta_IBAN INT NOT NULL,
+	constraint SALARIOS_PK PRIMARY KEY (idSalario));
+
+CREATE sequence CLIENTES_IDCLIENTE_SEQ;
+
+CREATE trigger BI_CLIENTES_IDCLIENTE
+  before insert on Clientes
+  for each row
+begin
+  select CLIENTES_IDCLIENTE_SEQ.nextval into :NEW.idCliente from dual;
+end;
+
+CREATE sequence MATRICULA_IDMATRICULA_SEQ;
+
+CREATE trigger BI_MATRICULA_IDMATRICULA
+  before insert on Matricula
+  for each row
+begin
+  select MATRICULA_IDMATRICULA_SEQ.nextval into :NEW.idMatricula from dual;
+end;
+
+CREATE sequence SUSCRIPCIONES_IDSUSCRIPCION_SEQ;
+
+CREATE trigger BI_SUSCRIPCIONES_IDSUSCRIPCION
+  before insert on Suscripciones
+  for each row
+begin
+  select SUSCRIPCIONES_IDSUSCRIPCION_SEQ.nextval into :NEW.idSuscripcion from dual;
+end;
+
+CREATE sequence NUTRICION_IDNUTRICION_SEQ;
+
+CREATE trigger BI_NUTRICION_IDNUTRICION
+  before insert on Nutricion
+  for each row
+begin
+  select NUTRICION_IDNUTRICION_SEQ.nextval into :NEW.idNutricion from dual;
+end;
+
+CREATE sequence STAFF_IDSTAFF_SEQ;
+
+CREATE trigger BI_STAFF_IDSTAFF
+  before insert on Staff
+  for each row
+begin
+  select STAFF_IDSTAFF_SEQ.nextval into :NEW.idStaff from dual;
+end;
+
+CREATE sequence HORARIOS_IDHORARIO_SEQ;
+
+CREATE trigger BI_HORARIOS_IDHORARIO
+  before insert on Horarios
+  for each row
+begin
+  select HORARIOS_IDHORARIO_SEQ.nextval into :NEW.idHorario from dual;
+end;
+
+CREATE sequence SALARIOS_IDSALARIO_SEQ;
+
+CREATE trigger BI_SALARIOS_IDSALARIO
+  before insert on Salarios
+  for each row
+begin
+  select SALARIOS_IDSALARIO_SEQ.nextval into :NEW.idSalario from dual;
+end;
+
+CREATE sequence CUENTAS_IBAN_IDCUENTA_SEQ;
+
+CREATE trigger BI_CUENTAS_IBAN_IDCUENTA
+  before insert on Cuentas_IBAN
+  for each row
+begin
+  select CUENTAS_IBAN_IDCUENTA_SEQ.nextval into :NEW.idCuenta from dual;
+end;
+
+ALTER TABLE Matricula ADD CONSTRAINT Matricula_fk0 FOREIGN KEY (Cliente) REFERENCES Clientes(idCliente);
+ALTER TABLE Matricula ADD CONSTRAINT Matricula_fk1 FOREIGN KEY (Suscripcion) REFERENCES Suscripciones(idSuscripcion);
+ALTER TABLE Matricula ADD CONSTRAINT Matricula_fk2 FOREIGN KEY (Nutricion) REFERENCES Nutricion(idNutricion);
+ALTER TABLE Suscripciones ADD CONSTRAINT Suscripciones_fk0 FOREIGN KEY (Entrenador) REFERENCES Staff(idStaff);
+ALTER TABLE Suscripciones ADD CONSTRAINT Suscripciones_fk1 FOREIGN KEY (Horario) REFERENCES Horarios(idHorario);
+ALTER TABLE Nutricion ADD CONSTRAINT Nutricion_fk0 FOREIGN KEY (Nutricionista) REFERENCES Staff(idStaff);
+ALTER TABLE Staff ADD CONSTRAINT Staff_fk0 FOREIGN KEY (Salario) REFERENCES Salarios(idSalario);
+ALTER TABLE Salarios ADD CONSTRAINT Salarios_fk0 FOREIGN KEY (Cuenta_IBAN) REFERENCES Cuentas_IBAN(idCuenta);
+ALTER TABLE Cuentas_IBAN ADD CONSTRAINT Cuentas_IBAN_fk0 FOREIGN KEY (Staff) REFERENCES Staff(idStaff);
+
