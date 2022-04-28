013d89fe3731c055ba00e8d33774561cfda7e7dc
Sebastian Zumbado H <sebastian.zumbado@ibm.com>
2022-04-28 17:48:45 -0600

    :pencil: Feat: Add test to get list of Staff

--- oracle_db/src/test/java/com/java_oracle/project/oracle_db/OracleDbApplicationTests.java
+++ oracle_db/src/test/java/com/java_oracle/project/oracle_db/OracleDbApplicationTests.java
@@ -1,13 +1,39 @@
 package com.java_oracle.project.oracle_db;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class OracleDbApplicationTests {
 
+	private static final Logger log = LoggerFactory.getLogger(OracleDbApplicationTests.class);
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	@DisplayName("Test 1: Get a JSON list of Staff ...............................")
+	void get_Staff_test() throws InterruptedException {
+		String command = "curl --location --request GET localhost:8181/staff/allStaff";
+		Process process = null;
+		try {
+			process = Runtime.getRuntime().exec(command);
+			log.info(process.getInputStream().toString());
+			
+		} catch (IOException e) {
+			log.error(e.toString());
+		}
+
+		assertEquals(0,process.waitFor(),"Command completed successfully.");
+		
 	}
 
 }
